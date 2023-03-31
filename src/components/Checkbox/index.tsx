a04d4e63705a9451c0503c080b486c4ad3605246
Shape Lee <shape1026@gmail.com>
2023-03-31 15:21:12 +0900

    Markup: Add Mock Data List

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import $ from './checkbox.module.scss'
+import Icon from '@components/Icon';
 
 const Checkbox = ({ id }: {id: number}) => {
+  const [isChecked, setChecked] = useState<string>('noneChecked')
+
   return (
     <label className={$.checkbox}>
       <input type={'checkbox'} />
+      <Icon kinds={isChecked}/>
     </label>
   );
 };
--- src/components/WordList/index.tsx
+++ src/components/WordList/index.tsx
@@ -4,27 +4,34 @@ import WordItem from '@components/WordItem'
 
 const MOCK_DATA = [
   {
-    id: 1,
     word: 'apple',
     text: '사과',
     example: 'i like apple',
-    timetamp: 1234567890123,
   },
   {
-    id: 2,
     word: 'banana',
     text: '바나나',
     example: 'i like banana',
-    timetamp: 1234567890123,
   },
 ]
+
+const MOCK_DATA_ARR = Array(10).fill({}).map((v, i) => {
+  const item = MOCK_DATA[Math.floor(Math.random() * 1)]
+  return {
+    id: 10 - i,
+    word: item.word,
+    text: item.text,
+    example: item.example,
+    timetamp: 1234567890123 + i,
+  }
+})
 
 const WordList = ({ isEdit }: { isEdit: boolean }) => {
   
   return (
     <ul className={$.word_list}>
       {
-        MOCK_DATA.map((item) => <WordItem {...item} isEdit={isEdit}/>)
+        MOCK_DATA_ARR.map((item) => <WordItem {...item} isEdit={isEdit}/>)
       }
     </ul>
   );
