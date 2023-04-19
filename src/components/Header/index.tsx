a2c9ed5d90052adc8557e50d86806f00a50601b1
Shape Lee <shape1026@gmail.com>
2023-04-19 21:08:36 +0900

    Markup: Add login page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,22 @@
 import React, { ReactNode } from 'react'
 import $ from './header.module.scss'
+import { useNavigate } from 'react-router-dom';
 
 const Header = ({ children }: { children: ReactNode }) => {
+  const navigate = useNavigate()
+
+  const goLogin = () => {
+    navigate('/login')
+  }
+
   return (
     <header className={$.header}>
       <h1>Word Book</h1>
       {children}
+      <div className={$.login_container}>
+        <div className={$.login} onClick={goLogin}>로그인</div>
+        <div className={$.join}>회원가입</div>
+      </div>
     </header>
   );
 };
