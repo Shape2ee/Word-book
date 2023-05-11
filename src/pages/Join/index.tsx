02dfb1e1419bafa98e7c72e6f3514b1f83454b29
Shape2ee <shape1026@gmail.com>
2023-05-11 16:50:41 +0900

    Markup: Add join inputs value & onChange

--- src/pages/Join/index.tsx
+++ src/pages/Join/index.tsx
@@ -1,33 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import $ from './join.module.scss'
 import Wrapper from '@components/Wrapper'
 import Button from '@components/Button'
 import Icon from '@components/Icon'
 
+interface JoinInputs {
+  joinId: string,
+  joinPw1: string,
+  joinPw2: string
+}
+
 const Join = () => {
+  
+  const [joinInputs, setJoinInputs] = useState<JoinInputs>({
+    joinId: '',
+    joinPw1: '',
+    joinPw2: ''
+  })
+
+  const { joinId, joinPw1, joinPw2 } = joinInputs
+
+  const handleJoinInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.name)
+    setJoinInputs((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  const handleJoinSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(joinInputs)
+  }
+
   return (
     <Wrapper>
       <div className={$.join_container}>
-        <form>
+        <form onSubmit={handleJoinSubmit}>
           <div className={$.input_row}>
             <h3 className={$.join_title}>
-              <label htmlFor='id'>아이디</label>
+              <label htmlFor='joinId'>아이디</label>
             </h3>
             <span className={$.input_box}>
-              <input type='text' name='id' id='id'/>
+              <input type='text' name='joinId' id='joinId'
+                value={joinId} onChange={handleJoinInputChange}/>
+                <Button>
+                  <Icon kinds='cancell'/>
+                </Button>
             </span>
             <span>필수 정보입니다.</span>
             <span>5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.</span>
           </div>
           <div className={$.input_row}>
             <h3 className={$.join_title}>
-              <label htmlFor='password1'>비밀번호</label>
+              <label htmlFor='joinPw1'>비밀번호</label>
             </h3>
             <span className={$.input_box}>
-              <input type='text' name='password1' id='password1'/>
+              <input type='text' name='joinPw1' id='joinPw1'
+                value={joinPw1} onChange={handleJoinInputChange}/>
               <span className={$.confirm_box}>
                 <span>사용불가</span>
-                <Icon kinds='shieldX'/>
+                <Icon kinds='shieldFillX'/>
               </span>
             </span>
             <span>필수 정보입니다.</span>
@@ -35,10 +70,11 @@ const Join = () => {
           </div>
           <div className={$.input_row}>
             <h3 className={$.join_title}>
-              <label htmlFor='password2'>비밀번호 확인</label>
+              <label htmlFor='joinPw2'>비밀번호 확인</label>
             </h3>
             <span className={$.input_box}>
-              <input type='text' name='password2' id='password2'/>
+              <input type='text' name='joinPw2' id='joinPw2'
+                value={joinPw2} onChange={handleJoinInputChange}/>
               <span>확인</span>
             </span>
             <span>필수 정보입니다.</span>
