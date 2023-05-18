d65f9a3cce9d5cad48a6edfa484e34ff7f6b39af
Shape2ee <shape1026@gmail.com>
2023-05-18 21:20:26 +0900

    Docs: Change API baseURL

--- src/api/Fetcher.ts
+++ src/api/Fetcher.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { METHOD } from '@customTypes/CustumTypes'
 
-axios.defaults.baseURL = 'https://port-0-node-word-book-7hqac2alhjwi8bq.sel4.cloudtype.app/'
+axios.defaults.baseURL = 'https://port-0-node-word-book-7hqac2alhjwi8bq.sel4.cloudtype.app'
 // axios.defaults.baseURL = 'http://localhost:8000'
 
 export const fetcher = async (method: METHOD, url: string, ...rest: { [key: string]: any }[]) => {
